b8fc4264e71f5b48a3bd028878f56c0b2577b2da
francolind <90268531+francolind@users.noreply.github.com>
2022-12-04 11:32:00 -0300

    añadiendo responsing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,24 @@
 
 import search from '../assets/search.png';
 import Logo from '../assets/headerLogo.png';
+import menu from '../assets/menuW.png';
+import close from '../assets/closeW.png';
+import { useState } from 'react';
+
 
 export const Header = () => {
+    const [click,setClick] = useState(false);
+    const handleclick = () =>{
+        setClick(!click);
+    }
+    
     return(
             <header className="header-conteiner">
                 <div className="header-logo">
-                    <img src={headerLogo} alt="Logo" />
+                    <img src={Logo} alt="Logo" />
                     <p className='logo-title'>Coinbyte</p>
                 </div>
-                <div className="header-menu">
+                <div className={ click ? 'header-menu' : 'menu-desactive'}>
                     <ul>
                         <li>home</li>
                         <li>about</li>
@@ -18,7 +27,12 @@ export const Header = () => {
                         <li className='li-boton'>get started</li>
                     </ul>
                 </div>
-                <div className='header-search'>
+                <div onClick={handleclick} className='header-boton'>{
+                   click ?   <img className='menu' src={close} alt="" /> :
+                       <img className='close' src={menu} alt="" />
+                }
+                </div>
+                <div  className='header-search'>
                     <img src={search} alt="search" />
                 </div>
             </header> 
